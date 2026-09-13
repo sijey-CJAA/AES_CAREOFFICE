e621df08a8ec6d916d192fa85dd473207d66b311
Christopher Jr. A. Artuz <artuzjrchristopher@gmail.com>
2026-09-13 23:21:34 +0800

    feat: add sidebar navigation component

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -44,7 +44,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
             Deleted Records
         </a>
-        <a href="logout.php" class="nav-item">
+        <a href="/logout.php" class="nav-item">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
             Logout
         </a>
